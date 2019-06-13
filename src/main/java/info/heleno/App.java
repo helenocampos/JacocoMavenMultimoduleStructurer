de5e74dcf547b4ddf91549134810e5d98e4a4629
Heleno Campos <helenocampos@LAB-ESI-iMac-21.local>
2019-06-13 15:09:19 -0300

    Fix some bugs

--- src/main/java/info/heleno/App.java
+++ src/main/java/info/heleno/App.java
@@ -15,7 +15,6 @@ import java.text.Normalizer;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Contributor;
 import org.apache.maven.model.Dependency;
@@ -31,32 +30,28 @@ import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
  *
  * @author helenocampos
  */
-public class App
-{
+public class App {
 
     static String jacocoVersion = "0.8.2";
 
-    public static void main(String[] args)
-    {
-        if (args.length > 0)
-        {
+    public static void main(String[] args) {
+        if (args.length > 0) {
             String projectPath = args[0];
             Model parentPom = readPom(projectPath);
-            List<String> modulesArtifactId = new LinkedList<>();
+            List<Model> modules = new LinkedList<>();
             List<String> modulesFoldersName = parentPom.getModules();
             String groupIdModule = parentPom.getGroupId();
-            if (modulesFoldersName != null && !modulesFoldersName.isEmpty())
-            {
-                for (String moduleFolderName : modulesFoldersName)
-                {
+            if (modulesFoldersName != null && !modulesFoldersName.isEmpty()) {
+                for (String moduleFolderName : modulesFoldersName) {
                     File moduleFolder = new File(projectPath, moduleFolderName);
-                    if (moduleFolder.exists())
-                    {
+                    if (moduleFolder.exists()) {
                         Model modulePom = readPom(moduleFolder.getAbsolutePath());
-                        if (modulePom != null)
-                        {
-                            modulesArtifactId.add(modulePom.getArtifactId());
-                            groupIdModule = modulePom.getGroupId();
+                        if (modulePom != null) {
+                            modules.add(modulePom);
+                            if (modulePom.getGroupId() != null) {
+                                groupIdModule = modulePom.getGroupId();
+                            }
+
                         }
                     }
                 }
@@ -64,7 +59,7 @@ public class App
                 File aggregatorFolder = new File(projectPath, "aggregator");
                 aggregatorFolder.mkdir();
                 Model aggregatorModel = getAggregatorModelPom(parentPom.getArtifactId(),
-                        parentPom.getVersion(), parentPom.getGroupId(), modulesArtifactId, groupIdModule);
+                        parentPom.getVersion(), parentPom.getGroupId(), modules, groupIdModule);
                 writePom(aggregatorModel, aggregatorFolder.getAbsolutePath());
 
                 Plugin plugin = new Plugin();
@@ -80,8 +75,7 @@ public class App
                 build.setPlugins(removePlugin("jacoco-maven-plugin", build.getPlugins()));
                 build.addPlugin(plugin);
                 parentPom.setBuild(build);
-                if (!parentPom.getModules().contains("aggregator"))
-                {
+                if (!parentPom.getModules().contains("aggregator")) {
                     parentPom.addModule("aggregator");
                 }
 
@@ -92,22 +86,18 @@ public class App
 
     }
 
-    private static List<Plugin> removePlugin(String artifactId, List<Plugin> plugins)
-    {
+    private static List<Plugin> removePlugin(String artifactId, List<Plugin> plugins) {
         Iterator<Plugin> pluginsIterator = plugins.iterator();
-        while (pluginsIterator.hasNext())
-        {
+        while (pluginsIterator.hasNext()) {
             Plugin plugin = pluginsIterator.next();
-            if (plugin.getArtifactId().equals(artifactId))
-            {
+            if (plugin.getArtifactId().equals(artifactId)) {
                 pluginsIterator.remove();
             }
         }
         return plugins;
     }
 
-    public static Model getAggregatorModelPom(String parentId, String parentVersion, String parentGroupId, List<String> dependencies, String moduleGroupId)
-    {
+    public static Model getAggregatorModelPom(String parentId, String parentVersion, String parentGroupId, List<Model> dependencies, String moduleGroupId) {
         Model pomModel = new Model();
         pomModel.setModelVersion("4.0.0");
         Parent parent = new Parent();
@@ -115,12 +105,24 @@ public class App
         parent.setGroupId(parentGroupId);
         parent.setVersion(parentVersion);
         pomModel.setParent(parent);
-        for (String dependency : dependencies)
-        {
+        for (Model dependency : dependencies) {
             Dependency dep = new Dependency();
-            dep.setArtifactId(dependency);
-            dep.setGroupId("${project.groupId}");
-            dep.setVersion("${project.version}");
+            if (dependency.getGroupId() != null) {
+                dep.setGroupId(dependency.getGroupId());
+            } else {
+                dep.setGroupId(moduleGroupId);
+            }
+            if (dependency.getArtifactId() != null) {
+                dep.setArtifactId(dependency.getArtifactId());
+            } else {
+
+            }
+            if (dependency.getVersion() != null) {
+                dep.setVersion(dependency.getVersion());
+            } else {
+                dep.setVersion("${project.version}");
+            }
+
             pomModel.addDependency(dep);
         }
         pomModel.setArtifactId("aggregator");
@@ -145,21 +147,16 @@ public class App
         return pomModel;
     }
 
-    public static Model readPom(String projectFolder)
-    {
+    public static Model readPom(String projectFolder) {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         File newProjectDir = new File(projectFolder);
         Model pomModel = null;
-        if (newProjectDir.exists())
-        {
+        if (newProjectDir.exists()) {
             File pom = new File(newProjectDir, "pom.xml");
-            if (pom.exists())
-            {
-                try
-                {
+            if (pom.exists()) {
+                try {
                     pomModel = reader.read(new FileReader(pom));
-                } catch (Exception ex)
-                {
+                } catch (Exception ex) {
 //                    Logger.getLogger(MyMojo.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
@@ -167,53 +164,41 @@ public class App
         return pomModel;
     }
 
-    public static void writePom(Model model, String projectFolder)
-    {
+    public static void writePom(Model model, String projectFolder) {
         File newProjectDir = new File(projectFolder);
-        if (newProjectDir.exists())
-        {
+        if (newProjectDir.exists()) {
             File pom = new File(newProjectDir, "pom.xml");
 
-            try
-            {
+            try {
                 DefaultModelWriter writer = new DefaultModelWriter();
                 OutputStream output = new FileOutputStream(pom);
                 replaceSpecialCharacters(model);
                 writer.write(output, null, model);
                 output.close();
-            } catch (FileNotFoundException ex)
-            {
+            } catch (FileNotFoundException ex) {
 //                    Logger.getLogger(MyMojo.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex)
-            {
+            } catch (IOException ex) {
 //                    Logger.getLogger(MyMojo.class.getName()).log(Level.SEVERE, null, ex);
             }
 
         }
     }
 
-    private static void replaceSpecialCharacters(Model model)
-    {
+    private static void replaceSpecialCharacters(Model model) {
         List<Contributor> contributors = model.getContributors();
-        if (contributors != null)
-        {
-            for (Contributor contributor : contributors)
-            {
+        if (contributors != null) {
+            for (Contributor contributor : contributors) {
                 String name = contributor.getName();
-                if (name != null)
-                {
+                if (name != null) {
                     contributor.setName(Normalizer.normalize(name, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", ""));
                 }
             }
         }
         List<Developer> developers = model.getDevelopers();
-        if (developers != null)
-        {
-            for (Developer developer : developers)
-            {
+        if (developers != null) {
+            for (Developer developer : developers) {
                 String name = developer.getName();
-                if (name != null)
-                {
+                if (name != null) {
                     developer.setName(Normalizer.normalize(name, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", ""));
                 }
             }
